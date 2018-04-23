dd00dba5d9e5d97779aa7cdefe7b0628c342c694
qicaitiankong <zihaoshuhan@163.com>
2018-04-23 20:32:05 +0800

    Initial Commit

---
+++ testLzh4.23/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testLzh4.23
+//
+//  Created by 李自豪 on 2018/4/23.
+//  Copyright © 2018年 haichuanhuiwulianxinxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testLzh4.23/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testLzh4.23
+//
+//  Created by 李自豪 on 2018/4/23.
+//  Copyright © 2018年 haichuanhuiwulianxinxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
